70c9efa2b0621802cd756b9adff8bf3408faded6
xukaiqiang <>
2020-11-05 16:58:25 +0800

    11

---
+++ js/common.js
@@ -0,0 +1,96 @@
+//jqGrid的配置信息
+$.jgrid.defaults.width = 1000;
+$.jgrid.defaults.responsive = true;
+$.jgrid.defaults.styleUI = 'Bootstrap';
+
+var baseURL = "../../";
+
+//工具集合Tools
+window.T = {};
+
+// 获取请求参数
+// 使用示例
+// location.href = http://localhost:8080/index.html?id=123
+// T.p('id') --> 123;
+var url = function(name) {
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r!=null)return  unescape(r[2]); return null;
+};
+T.p = url;
+
+//全局配置
+$.ajaxSetup({
+	dataType: "json",
+    headers: {"Content-Type": "application/json;charset=utf8", "AuthToken": localStorage.getItem("AuthToken")},
+	cache: false
+});
+
+//重写alert
+window.alert = function(msg, callback){
+	parent.layer.alert(msg, function(index){
+		parent.layer.close(index);
+		if(typeof(callback) === "function"){
+			callback("ok");
+		}
+	});
+}
+
+//重写confirm式样框
+window.confirm = function(msg, callback){
+	parent.layer.confirm(msg, {btn: ['确定','取消']},
+	function(){//确定事件
+		if(typeof(callback) === "function"){
+			callback("ok");
+		}
+	});
+}
+
+//选择一条记录
+function getSelectedRow(grid) {
+    var rowKey = grid.getGridParam("selrow");
+    if(!rowKey){
+    	alert("请选择一条记录");
+    	return ;
+    }
+    
+    var selectedIDs = grid.getGridParam("selarrrow");
+    if(selectedIDs.length > 1){
+    	alert("只能选择一条记录");
+    	return ;
+    }
+    
+    return selectedIDs[0];
+}
+
+//选择一条记录
+function getSelectedRowData(grid,rowId) {
+    return grid.jqGrid('getRowData',rowId);
+}
+
+//选择多条记录
+function getSelectedRows(grip) {
+    var rowKey = grid.getGridParam("selrow");
+    if(!rowKey){
+    	alert("请选择一条记录");
+    	return ;
+    }
+    
+    return grid.getGridParam("selarrrow");
+}
+
+//判断是否为空
+function isBlank(value) {
+    return !value || !/\S/.test(value)
+}
+
+//构建url参数
+function buildUrl(url, param) {
+    url = url + "?";
+    for (var o in param) {
+        if (param[o] != -1&&param[o] != "") {
+            url += o + "=" + param[o] + "&";
+        }
+    }
+    return url.substring(0, url.length - 1);
+}
---
+++ js/index.js
@@ -0,0 +1,135 @@
+//生成菜单
+var menuItem = Vue.extend({
+    name: 'menu-item',
+    props:{item:{}},
+    template:[
+        '<li>',
+        '	<a v-if="item.menuType === 1" href="javascript:;">',
+        '		<i v-if="item.icon != null" :class="item.icon"></i>',
+        '		<span>{{item.menuName}}</span>',
+        '		<i class="fa fa-angle-left pull-right"></i>',
+        '	</a>',
+        '	<ul v-if="item.menuType === 1" class="treeview-menu">',
+        '		<menu-item :item="item" v-for="item in item.child"></menu-item>',
+        '	</ul>',
+
+        '	<a v-if="item.menuType === 2 && item.parentId === 0" :href="\'#\'+item.menuUrl">',
+        '		<i v-if="item.icon != null" :class="item.icon"></i>',
+        '		<span>{{item.menuName}}</span>',
+        '	</a>',
+
+        '	<a v-if="item.menuType === 2 && item.parentId != 0" :href="\'#\'+item.menuUrl">',
+        '		<i v-if="item.icon != null" :class="item.icon"></i>',
+        '		<i v-else class="fa fa-circle-o"></i> {{item.menuName}}',
+        '	</a>',
+        '</li>'
+    ].join('')
+});
+
+//iframe自适应
+$(window).on('resize', function() {
+	var $content = $('.content');
+	$content.height($(this).height() - 154);
+	$content.find('iframe').each(function() {
+		$(this).height($content.height());
+	});
+}).resize();
+
+//注册菜单组件
+Vue.component('menuItem',menuItem);
+
+var vm = new Vue({
+	el:'#rrapp',
+	data:{
+		uname:'',
+		menuList:{},
+		main:"main.html",
+		password:'',
+		newPassword:'',
+        navTitle:"控制台"
+	},
+	methods: {
+		getMenuList: function (event) {
+			$.getJSON("service/menu/listByUser", function(r){
+				if(r.code=="000000"){
+					vm.menuList = r.data;
+				}else if(r.code=="100002"){
+					parent.location.href = "login.html"
+				}
+			});
+		},
+		updatePassword: function(){
+			layer.open({
+				type: 1,
+				skin: 'layui-layer-molv',
+				title: "修改密码",
+				area: ['550px', '270px'],
+				shadeClose: false,
+				content: jQuery("#passwordLayer"),
+				btn: ['修改','取消'],
+				btn1: function (index) {
+					var data = "password="+vm.password+"&newPassword="+vm.newPassword;
+					$.ajax({
+						type: "POST",
+					    url: "sys/user/password",
+					    data: data,
+					    dataType: "json",
+					    success: function(result){
+							if(result.code == "000000"){
+								layer.close(index);
+								layer.alert('修改成功', function(index){
+									location.reload();
+								});
+							}else{
+								layer.alert(result.msg);
+							}
+						}
+					});
+	            }
+			});
+	},
+    donate: function () {
+	        layer.open({
+	            type: 2,
+	            title: false,
+	            area: ['806px', '467px'],
+	            closeBtn: 1,
+	            shadeClose: false,
+	            content: ['http://cdn.renren.io/donate.jpg', 'no']
+	        });
+    	}
+	},
+	created: function(){
+		this.getMenuList();
+	},
+	updated: function(){
+		//路由
+		var router = new Router();
+		routerList(router, vm.menuList);
+		router.start();
+	}
+});
+
+
+
+function routerList(router, menuList){
+	for(var key in menuList){
+		var menu = menuList[key];
+		if(menu.menuType == 1){
+			routerList(router, menu.child);
+		}else if(menu.menuType == 2){
+			router.add('#'+menu.menuUrl, function() {
+				var url = window.location.hash;
+				
+				//替换iframe的url
+			    vm.main = url.replace('#', '');
+			    
+			    //导航菜单展开
+			    $(".treeview-menu li").removeClass("active");
+			    $("a[href='"+url+"']").parents("li").addClass("active");
+			    
+			    vm.navTitle = $("a[href='"+url+"']").text();
+			});
+		}
+	}
+}
